2064a165b2f19b271b57e92523db868410807631
Mario <mperdiguerobarrera@safareyes.es>
2024-05-01 16:50:38 +0200

    ♻️ Update require libraries

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module mpb.control_de_personas {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+    requires java.desktop;
 
     opens apps to javafx.fxml;
     opens controller to javafx.fxml;
